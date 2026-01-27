f5f712f6176969099790e5b9a2b055e041bc357d
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-01-27 11:45:26 +0300

    updated

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,11 +1,18 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Cart, CartItem
 from .serializers import CartSerializer, CartItemSerializer
 
 class CartViewSet(viewsets.ModelViewSet):
-    queryset = Cart.objects.all()
     serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
 
 class CartItemViewSet(viewsets.ModelViewSet):
-    queryset = CartItem.objects.all()
     serializer_class = CartItemSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return CartItem.objects.filter(cart__user=self.request.user)
