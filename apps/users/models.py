966b034b852e4d087a69232b4776dd5f355e6346
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-03-09 16:36:15 +0300

    updated

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,9 +5,10 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)   
     phone_number = models.CharField(max_length=20, blank=True)
     profile_picture = models.ImageField(upload_to='profiles/', blank=True)
-    
+    username = models.CharField(max_length=150, blank=True, null=True)
     USERNAME_FIELD = 'email'  
-    REQUIRED_FIELDS = ['username']  
+    REQUIRED_FIELDS = []   
+
     
     def __str__(self):
         return self.email
