ea8ef10d15ee91e65ec78a73ee99584607f0cd83
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-03-10 16:58:05 +0300

    updated

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-wyjzmy4+88it92i8q-8xws*cmrc43yp=3)ma#b#!t0=)3saqqh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from apps.users.views import CustomTokenObtainPairView  # Import your custom view
+from apps.users.views import CustomTokenObtainPairView   
 
 urlpatterns = [
     path('admin/', admin.site.urls),
