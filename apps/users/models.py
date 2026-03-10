9e3800cb0aafa74ab6c959e9d97d5b8271191bd6
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-03-10 17:18:29 +0300

    updated

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)   
@@ -8,6 +25,8 @@ class User(AbstractUser):
     username = models.CharField(max_length=150, blank=True, null=True)
     USERNAME_FIELD = 'email'  
     REQUIRED_FIELDS = []   
+    objects = CustomUserManager()  
+
 
     
     def __str__(self):
